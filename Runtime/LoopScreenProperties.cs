6640850d7aaf9031932c50c9a7d500634a57f6ae
danielshapley <daniel.shapley@nimlok.co.uk>
2024-09-23 16:25:23 +0100

    changed default loop time

--- Runtime/LoopScreenProperties.cs
+++ Runtime/LoopScreenProperties.cs
@@ -6,7 +6,7 @@ namespace Nimlok.Screens
     [Serializable, BoxGroup("Loop Properties")]
     public class LoopScreenProperties
     {
-        public float loopTime = 5;
+        public float loopTime = 10;
         public TransitionableScreen nextScreen;
     }
 }
